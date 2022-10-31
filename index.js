e13e019f934a2b77b1db1935eb148979e0c874dc
Haotian Yan <hyan227@uwo.ca>
2022-10-31 17:40:57 -0400

    Testing the static file containing html and css

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
 const parts = [
     {id: 100, name: 'Belt', color: 'brown'},
     {id: 101, name: 'Clip', color: 'brown'},
     {id: 102, name: 'Belt', color: 'red'},
     {id: 103, name: 'Hat', color: 'Purple'},
 ];
+
+// Setup serving front-end code
+app.use('/', express.static('static'));
+
 app.get('/api/parts', (request, response)=> {
     console.log(`GET request for ${request.url}`);
     response.send(parts);
