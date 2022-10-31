8296c5eee91c9db45a4defd352b389c1cde47cf3
Haotian Yan <hyan227@uwo.ca>
2022-10-31 19:01:44 -0400

    Testing for input request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
 
 const parts = [
     {id: 100, name: 'Belt', color: 'brown'},
@@ -12,15 +14,22 @@ const parts = [
 // Setup serving front-end code
 app.use('/', express.static('static'));
 
-app.get('/api/parts', (request, response)=> {
-    console.log(`GET request for ${request.url}`);
+// Setup middileware to do logging
+app.use((request, respond, next)=> {
+    console.log(`${request.method} request for ${request.url}`);
+    next(); // keep going 
+})
+
+// Parse data in body as JSON
+router.use(express.json());
+
+router.get('/', (request, response)=> {
     response.send(parts);
 });
 
 // Get details for a given part
-app.get('/api/parts/:part_id', (request, respond)=>{
+router.get('/:part_id', (request, respond)=>{
     const id = request.params.part_id;
-    console.log(`GET request for ${request, respond}`);
     const part = parts.find(p => p.id === parseInt(id));
     if(part){
         respond.send(part);
@@ -30,6 +39,30 @@ app.get('/api/parts/:part_id', (request, respond)=>{
     }
 }
 );
+
+router.put('/:id', (request, respond)=>{
+    const newpart= request.body;
+    console.log("Part: ", newpart);
+    // Add ID field
+    newpart.id = parseInt(request.params.id);
+    // Replace the part with the new one.
+    const part = parts.findIndex(p => p.id === parseInt(newpart.id));
+    if(part < 0){
+        console.log('Creating new part');
+        parts.push (request.body);
+    }
+    else{
+        console.log('Modifying part', request.params.id);
+        parts[part] = request.body;
+    }
+
+    respond.send(newpart);
+});
+
+
+
+// Install the router at /api/parts
+app.use('/api/parts', router)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
