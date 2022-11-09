99fca37d2bbe79174302e35ffce02a8712d02dc4
Haotian Yan <hyan227@uwo.ca>
2022-11-09 18:55:30 -0500

    Adjust order for schema

--- schema.js
+++ schema.js
@@ -1,34 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const listSchema = new mongoose.Schema({
-
-    listname: String, 
-    id: String, 
-
+  listname: String,
+  id: String,
 });
 
 listSchema.statics.isList = async function (listname) {
+  if (!listname) throw new Error("Error 404");
 
-    if(!listname) throw new Error ('Error 404');
+  try {
+    const list = await this.findOne({ listname });
 
-    try{
-
-        const list = await this.findOne({listname});
-
-        if(list){
-            return false;
-        }
-        else{
-            return true;
-        }
-        
-    } catch (error) {
-
-        console.log("There is an Error");
-        console.log(error.message);
-        return false;
+    if (list) {
+      return false;
+    } else {
+      return true;
     }
+  } catch (error) {
+    console.log("There is an Error");
+    console.log(error.message);
+    return false;
+  }
+};
 
-}
-
-module.exports = mongoose.model('list', listSchema);
+module.exports = mongoose.model("list", listSchema);
